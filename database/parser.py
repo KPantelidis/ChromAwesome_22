3894d2378be1c4bfcfacb4160873650bfe14150a
Kostas Pantelidis <52851541+KPantelidis@users.noreply.github.com>
2020-05-03 11:15:26 +0100

    Update and rename parse_test.py to parser.py
    Updated cds_list, complement_list, translation_list, list_of_entries

--- database/parser.py
+++ database/parser.py
@@ -1,14 +1,11 @@
-import pandas as pd
 import re
-#from pprint import pprint
 
 my_list = []
-with open("bla", mode="r") as bla:
+with open('bla', mode="r") as bla:
     reader = bla.read()
     my_list = reader.split("//\n")
-  
-  
-j = 0
+
+ 
 list_of_entries = []
 accession_list = []
 gene_id_list = []
@@ -17,8 +14,7 @@ protein_id_list = []
 product_list = []
 translation_list = []
 
-for i in my_list:
-    j += 1    
+for i in my_list:    
 
     #-----------accession----------------
     p1 = re.compile(r'\bACCESSION \s+\w+\s')
@@ -31,7 +27,7 @@ for i in my_list:
     if p2.search(i):
         p2 = p2.findall(i)
         p2 = [i.replace('/gene="', '').replace('"', '').strip() for i in p2] 
-        p2 = list(dict.fromkeys(p2)) #for dublicates
+        p2 = list(dict.fromkeys(p2)) #for duplicates
         gene_id_list.append(p2[0])
     else:
         gene_id_list.append('None')
@@ -41,7 +37,7 @@ for i in my_list:
     if p3.search(i):
         p3 = p3.findall(i)
         p3 = [i.replace('/map="', '').replace('"', '').strip() for i in p3] 
-        p3 = list(dict.fromkeys(p3)) #for dublicates
+        p3 = list(dict.fromkeys(p3)) #for duplicates
         location_list.append(p3[0])
     else:
         location_list.append('None')
@@ -51,7 +47,7 @@ for i in my_list:
     if p10.search(i):
         p10 = p10.findall(i)
         p10 = [i.replace('/protein_id="', '').replace('"', '').strip() for i in p10] 
-        p10 = list(dict.fromkeys(p10)) #for dublicates
+        p10 = list(dict.fromkeys(p10)) #for duplicates
         protein_id_list.append(p10[0])
     else:
         protein_id_list.append('None')
@@ -61,38 +57,35 @@ for i in my_list:
     if p11.search(i):
         p11 = p11.findall(i)
         p11 = [i.replace('/product="', '').replace('"', '').strip() for i in p11] 
-        p11 = list(dict.fromkeys(p11)) #for dublicates
+        p11 = list(dict.fromkeys(p11)) #for duplicates
         product_list.append(p11[0])
     else:
         product_list.append('None')   
-        
-    #-----------translation----------------(chech the dublicates)
-    p12 = re.compile(r'\/translation="(.[^"]*?)"\s')
+       
+#-----------translation----------------    
+    p12 = re.compile(r'/translation="(.[^"]*?)"\s')
     if p12.search(i):
         p12=p12.findall(i)
-        p12 = [i.replace('\n', '').strip() for i in p12]
-        p12 = list(dict.fromkeys(p12)) #for dublicates
-        translation_list.append(p12[0])
+        p12 = [i.replace('\n', '').replace(' ','').strip() for i in p12]
+        p12 = list(dict.fromkeys(p12)) #for duplicates
+        translation_list.append(p12[0]) 
     else:
         translation_list.append('None')
-
-#---------dna_sequence---------      
+#print(translation_list)
+        
+#---------dna_sequence--------- 
+# no need to check for duplicates, there's only one origin per entry
 dna_list =[]      
 for i in my_list:
+    dna = []
     p4 = re.compile(r'(ORIGIN\s+)((.*\n){1,})')
-    if p4.search(i):
-        p4 = p4.findall(i)
-        p4 = re.compile(r'(ORIGIN\s+)((.*\n){1,})')
-        it = p4.finditer(i)
-        for match in it:
-            dna_list.append(match.group(2))
-            dna_list = [i.replace('\n', '').replace(' ', '').strip() for i in dna_list]
-            dna_list = [re.sub('[0-9]', '', i) for i in dna_list] 
-        dna_list += dna_list
-    else:
-        dna_list.append('None')
-# no need to check for doublucates, there's only one origin per entry
-    
+    it = p4.finditer(i)
+    for match in it:
+        dna.append(match.group(2))
+        dna = [i.replace('\n', '').replace(' ', '').strip() for i in dna]
+        dna = [re.sub('[0-9]', '', i) for i in dna] 
+    dna_list += dna
+#print(dna_list)   
      
 #----------gene_bp------------------
 gene_bp_list = []
@@ -108,30 +101,45 @@ for i in my_list:
         gene_bp_list += gene_bp
     else:
         gene_bp_list.append('None')
-#no need to check for doublicates, i take it from LOCUS line
+#no need to check for duplicates, i take it from LOCUS line
 
-'''
-#----------CDS_start---------    
-cds_start_list = []
+
+#----------CDS---------    
+cds_list = []
 for i in my_list:
-    cds_start = []
-    p7 = re.compile(r'(\bCDS)\s+(\w+)?(\W+)(\d+)')
-    it = p7.finditer(i)
-    for match in it:
-        cds_start.append(match.group(4))
-    cds_start_list += cds_start
-#print(cds_start_list)
-'''
+    p7 = re.compile(r'CDS\s*(.[^\/]*?)\/')
+    if p7.search(i):
+        p7 = p7.findall(i)
+        p7 = [i.replace('\n', '')
+                .replace(' ', '')
+                .replace('join', '')
+                .replace('complement','')
+                .strip() for i in p7]
+        cds_list.append(p7[0])
+    else:
+        cds_list.append('None')
+
+#------- complement-----------
+complement_list = []
+for i in my_list:
+    p8 = re.compile(r'CDS\s+complement')
+    if p8.search(i):
+        p8 = p8.findall(i)
+        complement_list.append('COMPLEMENT')
+    else:
+        complement_list.append('NO')
+        
+
+
 
 list_of_entries = list(zip(accession_list,
+                               gene_bp_list,
                                gene_id_list,
                                location_list,
-                               gene_bp_list,
+                               dna_list,
+                               cds_list,
+                               complement_list,
                                protein_id_list,
                                product_list,
                                translation_list
                                ))
-#print(list_of_entries)
-
-def bla():
-    return list_of_entries
