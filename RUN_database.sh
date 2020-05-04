2f51533ff18cc3b4725000821171f40bd9fa8be3
Kostas Pantelidis <52851541+KPantelidis@users.noreply.github.com>
2020-05-04 23:48:22 +0100

    Update RUN_database.sh

--- RUN_database.sh
+++ RUN_database.sh
@@ -1,16 +1,17 @@
-#!/bin/bash 
+#!/usr/bin/env python3
 
-## Log in into database
 ## Download and extract the data (GenBank file)
 ## Parse the GenBank file
 ## Populate the database
 
 
 echo "## Download and extract the data (GenBank file)"
-wget http://www.bioinf.org.uk/teaching/bbk/biocomp2/project/data/chrom_CDS_22.gz -O database/chrom_CDS_22.gz
+wget wget http://www.bioinf.org.uk/teaching/bbk/biocomp2/project/data/chrom_CDS_22.gz -O database/chrom_CDS_22.gz
 
 echo "## Parse GeneBank file"
+chmod +x database/parser.py
 python database/parser.py
 
 echo "## Populate Database"
+chmod +x database/create_table_and_populate.py
 python database/create_table_and_populate.py
