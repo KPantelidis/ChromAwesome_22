d22efd60e6aee26092e9e4396e4cb96e4b3d22a7
Kostas Pantelidis <52851541+KPantelidis@users.noreply.github.com>
2020-05-04 18:05:59 +0100

    Update RUN_database.sh

--- RUN_database.sh
+++ RUN_database.sh
@@ -9,7 +9,7 @@ echo "## Log in into database"
 mysql -u pk001 -p'wbjcod3k6' pk001
 
 echo "## Download and extract the data (GenBank file)"
-wget http://www.bioinf.org.uk/teaching/bbk/biocomp2/project/data/chrom_CDS_22.gz -O database
+wget http://www.bioinf.org.uk/teaching/bbk/biocomp2/project/data/chrom_CDS_22.gz -O database/chrom_CDS_22.gz
 
 echo "## Parse GeneBank file"
 python database/parser.py
