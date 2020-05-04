c64198e0cfc95d65a6ddc82cccbe4bcbbd0d23f5
Kostas Pantelidis <52851541+KPantelidis@users.noreply.github.com>
2020-05-04 23:00:54 +0100

    Update RUN_database.sh

--- RUN_database.sh
+++ RUN_database.sh
@@ -5,8 +5,6 @@
 ## Parse the GenBank file
 ## Populate the database
 
-echo "## Log in into database"
-mysql -u pk001 -p'wbjcod3k6' pk001
 
 echo "## Download and extract the data (GenBank file)"
 wget http://www.bioinf.org.uk/teaching/bbk/biocomp2/project/data/chrom_CDS_22.gz -O database/chrom_CDS_22.gz
