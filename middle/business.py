b61424f3281c240d8a3c7ac93edc0da43032630f
anniecp <62023232+anniecp@users.noreply.github.com>
2020-05-05 11:37:32 +0100

    Update business.py
    updated with new functions and new DB module names. Some functions amended.

--- middle/business.py
+++ middle/business.py
@@ -1,18 +1,24 @@
-import API
-import API_Column
+import dbapi
+import dbapi_column
 from Bio import Restriction
 from Bio.Seq import Seq
 from Bio.Alphabet.IUPAC import IUPACAmbiguousDNA
 from Bio.Alphabet import IUPAC
 import re
+import pymysql
+import pymysql.cursors
 
 #Create function that will store user input search query and search the database for
 #specfic row of data
 
 def search_db():
-    '''will search DB for query value, either accession, gene_id, location or protein_id and return a dictionary with the search outcome'''
+    '''will search DB for query value, either accession, gene_id, location or protein_id and return a dictionary with the search outcome
+    
+    param: nil - an input prompt will appear to insert a value
+    return: a dictionary of all raw DB information'''
+    
     Search_Query = input("Please enter a valid accession, gene_id, location or protein_id: ")
-    Database_Result = API.dict_entries(Search_Query)
+    Database_Result = dbapi.dict_entries(Search_Query)
     return(Database_Result)
    
 
@@ -21,11 +27,21 @@ def column_list(name):
     '''takes a column name and will return all non-duplicated values and
     remove all 'None' values
 
-    param name: name of a column in SQL table
+    param name: name of a column in SQL table, column names to choose from are either accession, gene_id, location or protein_id
     returns: a list of entries from column '''
 
-    newlist = API_Column.return_column(name)
-    return(newlist)
+    infolist = list(dbapi_column.return_column(name))
+    newlist = []
+    for elem in infolist:
+      if elem not in newlist:
+        newlist.append(elem)
+    #change from list of tuples to list    
+    endlist = [a for b in newlist for a in b] 
+    remove = ['None']
+    for elem in list(endlist):
+      if elem in remove:
+        endlist.remove(elem)
+    return(endlist)
 
 
 ## Create a function to identify RE enzyme sites in the DNA sequence
@@ -39,7 +55,9 @@ def search_RE(Search_Query, RE_name):
     returns: the location of the searched enzyme in the sequence '''
     
     amb = IUPACAmbiguousDNA()
-    Database_Result = API.dict_entries(Search_Query)
+    Database_Result = dbapi.dict_entries(Search_Query)
+    if Database_Result == False:
+      return('Please enter valid search value for either accession, location, gene_id or protein_id')
     DNA_seq = Database_Result['dna_seq']
     DNASeq = Seq(DNA_seq, amb)
 
@@ -67,7 +85,7 @@ def search_RE(Search_Query, RE_name):
 
 
 #create a function to mark up the CDS regions on the gene DNA seq
-def CDS_MarkUp(Search_Query):
+def CDS_markup(Search_Query):
     '''This function will take a DNA sequence, identify CDS sites and produce a marked up sequence
     that identifies one or more CDS sites in the DNA sequence.
 
@@ -75,7 +93,9 @@ def CDS_MarkUp(Search_Query):
     return: marked up sequence in string format
     '''
     #Take the search term and search database for corresponding information
-    Database_Result = API.dict_entries(Search_Query)
+    Database_Result = dbapi.dict_entries(Search_Query)
+    if Database_Result == False:
+      print('Please enter valid search value for either accession, location, gene_id or protein_id')
     
     p = re.compile(r'(\d+)..(\d+),?')
     CDS = Database_Result['cds']
@@ -106,11 +126,11 @@ def CDS_DNA(Search_Query):
     param: user search query
     return: list of CDS DNA sequence '''
     
-    CDS_Data = CDS_MarkUp(Search_Query)
+    CDS_Data = CDS_markup(Search_Query)
     p2 = re.compile(r'\<(\w+)\>')
     CDS_Seq = p2.findall(CDS_Data)
     return(CDS_Seq)
- 
+
 
 #Create a function to translate the CDS list into the translated protein seq
 def CDS_Protein(Search_Query):
@@ -153,7 +173,9 @@ def codon_freq(Search_Query):
     param: user search query
     return: dictionary of codons and their frequencies '''
 
-    Database_Result = API.dict_entries(Search_Query)
+    Database_Result = dbapi.dict_entries(Search_Query)
+    if Database_Result == False:
+      print('Please enter valid search value for either accession, location, gene_id or protein_id')
     DNA_seq = Database_Result['dna_seq']
     
     #create a list of DNA seq split into groups of 3 non-overlapping nucleotides
@@ -178,8 +200,9 @@ def CDS_RE(Search_Query, RE_Site):
     param: user search query, resriction enzyme name
     return: list of RE locations in seq '''
     CDS_Seq = CDS_DNA(Search_Query)
+    print(CDS_Seq)
     if CDS_Seq == []:
-        return ('No CDS sites have been identified in DNA')
+        print ('No CDS sites have been identified in DNA')
     
     for elem in CDS_Seq:
         amb = IUPACAmbiguousDNA()
@@ -187,20 +210,99 @@ def CDS_RE(Search_Query, RE_Site):
         if RE_Site == 'EcoRI':
             ecori = Restriction.EcoRI.search(search)
             if ecori == []:
-                return('There are no EcoRI locations in CDS region',elem)
+                print('There are no EcoRI locations in CDS region',elem)
             else:
-                return('The locations for EcoRI RE Site in CDS region',elem, 'are:', ecori)
+                print('The locations for EcoRI RE Site in CDS region',elem, 'are:', ecori)
         elif RE_Site == 'BamHI':
             bamhi = Restriction.BamHI.search(search)
             if bamhi == []:
-                return('There are no BamHI locations in CDS region',elem)
+                print('There are no BamHI locations in CDS region',elem)
             else:
-                return('The locations for BamHI RE Site in CDS region',elem, 'are:', bamhi)
+                print('The locations for BamHI RE Site in CDS region',elem, 'are:', bamhi)
         elif RE_Site == 'BsuRI':
             bsuri = Restriction.BsuRI.search(search)
             if bsuri == []:
-                return('There are no BsuRI locations in CDS region',elem)
+                print('There are no BsuRI locations in CDS region',elem)
             else:
-                return('The locations for BsuRI RE Site in CDS region',elem, 'are:', bsuri)
+                print('The locations for BsuRI RE Site in CDS region',elem, 'are:', bsuri)
         else:
-            return('CDS_RE function error: invalid search term or RE name')
+            print('CDS_RE function error: invalid search term or RE name')
+            
+            
+#create function to determine the codon frequeny in the whole chromosome
+def DNA_Codon():
+    '''Will take the DNA sequence across the whole chromosome and calculate codon frequencies
+    and return a populated dictionary.
+    
+    return: a dictionary populated with codon frequencies '''
+    
+    DNA_list = list(dbapi_column.return_column('dna_seq'))
+    DNA = [a for b in DNA_list for a in b] 
+    DNA_str = ""
+    for elem in DNA:
+      DNA_str += elem
+      
+    p5 = re.compile(r'\w\w\w') 
+    codon = p5.findall(DNA_str)
+
+    #Create a dictionary of codons and populate with the frequency of each codon
+    codon_dict = {}
+    for elem in codon:
+        if elem in codon_dict:
+            codon_dict[elem] += 1
+        else:
+            codon_dict[elem] = 1
+            
+     #turn dictionary into zipped lists for storage in database
+     
+    codon_value = [codon_dict[key] for key in sorted(codon_dict.keys())]
+    codon_key = list(sorted(codon_dict.keys()))
+    print(codon_value)
+    print(codon_key)
+    
+    codon_list = list(zip(codon_value, codon_key))
+    print(codon_list)
+    
+    config_dict = {'host' :'hope',
+               'user' : 'pk001',  
+               'password'   : 'wbjcod3k6',   
+               'port'     : 3306}
+
+    conn = pymysql.connect(**config_dict)
+
+    c = conn.cursor()
+    c.execute("DROP TABLE IF EXISTS pk001.dna_codon")
+    c.execute("CREATE TABLE pk001.dna_codon \
+              (frequency INT, \
+              codon VARCHAR(10)) ENGINE=InnoDB")
+    
+    query = "INSERT INTO pk001.dna_codon VALUES(%s,%s)"
+    c.executemany(query,codon_list)
+                 
+    conn.commit
+    print("Table 'dna_codon' created and data inserted successfully")   
+    conn.commit()
+    conn.close()        
+    return(codon_dict)
+    
+    
+def get_DNA_codon():
+    '''This function will return the codon frequency for the whole chromosome DNA sequence that 
+    has been calculated and stored in the database.
+    
+    param: NIL
+    return: list of tuples containing the codon and frequency '''
+
+    config_dict = {'host' :'hope',
+               'user' : 'pk001',  
+               'password'   : 'wbjcod3k6',   
+               'port'     : 3306}
+
+    conn = pymysql.connect(**config_dict)
+
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM pk001.dna_codon")
+    DNA_codon = list(c.fetchall())
+    return(DNA_codon)
+    
